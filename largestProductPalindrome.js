9addb8ccf3e0af3a6f132669000f02ef55bc4f6a
Patrick Xie <Patrick.y.xie@gmail.com>
2017-03-05 15:22:17 -0800

    progress

---
+++ largestProductPalindrome.js
@@ -0,0 +1,50 @@
+
+
+
+for(var i = 100; i > 0 ; i --){
+    if(i % 10 ===0){
+        var nums = numbersToMultiply(i)
+    }
+
+    for(var j=nums[0]; j>nums[1]; j--){
+        let r = i*j
+    }
+}
+
+numbersToMultiply(100);
+100, 90
+
+function numbersToMultiply(currentNum){
+    let tensCounted = (a - currentNum) /10
+    b - tensCounted;
+    let start = 100
+    let end = 100 - tensCounted*10
+    return [start, end]
+}
+
+
+function switch(){
+
+}
+
+
+//for first 10 numbers
+//multiply each by first 10 number of b
+//for second 10 numbers
+//multiple each by first 10 numbers of b, and 2nd 10 numbers of b
+//for third 10 numbers
+//multtiple each by first 10 numbers of b, and 2nd 10 numbers of b, and third 10 numbers of b
+
+
+
+
+var largestPalindrome = function(n) {
+    var a = '1' + new Array(n).fill('0').join('')
+    a = parseInt(a);
+    var b = a;
+    
+
+};
+
+
+console.log(largestPalindrome(2));
