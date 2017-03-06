fb597c66aa12acc07c3d82976a518112eb9d0353
Patrick Xie <Patrick.y.xie@gmail.com>
2017-03-05 21:24:41 -0800

    need to rewrite selenium in node due to lack of promise/callback

--- manipulatePage.py
+++ manipulatePage.py
@@ -1,12 +1,47 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+# from threading import Timer
+
+def send():
+    print "sending keys"
+    elem.send_keys("pycondef")
+
 
 driver = webdriver.Firefox()
 driver.get("http://localhost:8000/")
-assert "Python" in driver.title
-elem = driver.find_element_by_id("xx")
+# driver.get("https://ace.c9.io/#nav=embedding")
+# assert "Python" in driver.title
+script = "var e = document.getElementsByClassName(\'ace_line\');while(e.length > 0){ e[0].parentNode.removeChild(e[0]);}"
+elem = driver.find_element_by_class_name("ace_text-input")
 elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
+# driver.execute_script(script)
+# driver.implicitly_wait(25)
+try:
+    # element = WebDriverWait(driver, 10).until(
+    #     EC.presence_of_element_located((By.ID, "myDynamicElement"))
+    # )
+    WebDriverWait(driver, 10).until(driver.execute_script(script))
+
+finally:
+    send();
+
+
+# send()
+# elem.send_keys("pycondef")
+# t = Timer(20, send)
+# t.start()
+# elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
 driver.close()
+
+
+''' 
+className = ace_layer ace_text-layer
+execute a javascript to delete all the nodes with that specific classname
+and then send in the Keys
+
+className = ace_line are what we are trying to delete
+
+
+'''
+
--- test1.js
+++ test1.js
@@ -29,3 +29,7 @@ console.log(base64.decode(b))
 
 var a = "<p>Using <a href=\"https://py.checkio.org/mission/fizz-buzz/publications/StefanPochmann/python-27/shortest-52-kill-me-now/\" rel=\"nofollow\">my old CheckiO solution</a>:</p>\n<pre><code>def fizzBuzz(self, n):\n    return[&#39;FizzBuzz&#39;[i%-3&amp;-4:i%-5&amp;8^12]or`i`for i in range(1,n+1)]\n</code></pre>\n<p>Maybe I could shorten it to use <code>range(n)</code>, but as you can tell from my above link, that was exhausting enough :-)</p>\n<p>And a cleaner one I once saw somewhere:</p>\n<pre><code>def fizzBuzz(self, n):\n    return [&#39;Fizz&#39; * (not i % 3) + &#39;Buzz&#39; * (not i % 5) or str(i) for i in range(1, n+1)]</code></pre>\n"
 
+
+
+
+var e = document.getElementsByClassName('ace_line');while(e.length > 0){ e[0].parentNode.removeChild(e[0]);}
