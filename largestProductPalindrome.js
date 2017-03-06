bad29121c425dc8574ff94b81c690658073f4d03
Patrick Xie <Patrick.y.xie@gmail.com>
2017-03-05 16:14:34 -0800

    finished largest palindrome

--- largestProductPalindrome.js
+++ largestProductPalindrome.js
@@ -19,40 +19,89 @@
 
 
 
-var largestPalindrome = function(n) {
-    var a = '1' + new Array(n).fill('0').join('')
-    a = parseInt(a);
-    var b = a;
+// var largestPalindrome = function(n) {
+//     var a = '1' + new Array(n).fill('0').join('')
+//     a = parseInt(a);
+//     var b = a;
     
-//  var a =100;
-//  var b= 100;
-    function numbersToMultiply(currentNum){
-        // debugger;
-        // if(currentNum)
-        let tensCounted = (a - currentNum) /10
-        b - tensCounted;
-        let start = a
-        let end = b - tensCounted*10 - 10
-        return [start, end]
-    }
+//     function numbersToMultiply(currentNum){
+//         let tensCounted = (a - currentNum) /10
+//         b - tensCounted;
+//         let start = a
+//         let end = b - tensCounted*10 - 10
+//         return [start, end]
+//     }
 
-    for(var i = a; i > 0 ; i --){
-        // debugger
-        if(i % 10 ===0){
-            var nums = numbersToMultiply(i)
-        }
+//     for(var i = a; i > 0 ; i --){
+//         if(i % 10 ===0){
+//             var nums = numbersToMultiply(i)
+//         }
 
-        for(var j=nums[0]; j>nums[1]; j--){
-            let r = i*j
-            if (r.toString() === r.toString().split('').reverse().join('') ) {
-              console.log('r: %s, i: %s, j: %s', r, i, j)
-              return r % 1337;
+//         for(var j=nums[0]; j>nums[1]; j--){
+//             let r = i*j
+//             console.log('r: %s, i: %s, j: %s', r, i, j)
+//             if (r.toString() === r.toString().split('').reverse().join('') ) {
+              
+//               return r % 1337;
+//             }
+//         }
+
+//         if(i=== 910){
+//             return 'donezo'
+//         }
+//     }
+
+//     // var largest;
+//     // for(var i = a; i > 0 ; i--){
+//     //   for(var j=b; j > 0; j--){
+
+//     //     let r = i * j;
+//     //     if (r.toString() === r.toString().split('').reverse().join('') ){
+//     //       console.log('found r: ', r, i, j)
+          
+//     //     if (!largest||r > largest) {largest = r};
+//     //     }
+//     //   }
+//     // }
+//     // console.log(largest);
+//     // return largest % 1337;
+
+// };
+
+
+var largestPalindrome = function(n) {
+    // debugger
+    var max = Math.pow(10, n) - 1;
+    var min = Math.pow(10, n-1);
+    var pal = 0;
+    for (var i = max; i >= min; i--) {
+        for (var j = max; j >= i; j--) {
+            var result = i * j;
+            console.log(`r:${result}, i:${i}, j:${j}`)
+            if (result > pal) {
+                var str = result.toString();
+                var rStr = str.split("").reverse().join("");
+                if (str == rStr) {
+                    pal = result;
+                    break;
+                }
+            }else{
+                console.log("WE BROKE1")
+                break;
             }
         }
+    	if(j == max) {
+            console.log("WE BROKE2")
+    	    break;
+    	}
+        if(i===900){
+            return 'donezo'
+        }
     }
-
-
+    return (pal % 1337);
 };
 
-
-console.log(largestPalindrome(3));
+largestPalindrome(3)
+// 993 * 913, 906609
+console.log(largestPalindrome(3)===123);
+// 906609, 123
